a243474233e9f7619008c27909a890295b1201b8
nspenna <nicolas.penna16@gmail.com>
2025-08-06 23:53:59 -0300

    First model for source views

---
+++ dbtlearn/models/src/src_reviews.sql
@@ -0,0 +1,14 @@
+WITH raw_reviews AS (
+    SELECT
+        *
+    FROM
+        AIRBNB.RAW.raw_reviews
+)
+SELECT
+    listing_id,
+    reviewer_name,
+    date as review_date,
+    comments as review_text,
+    sentiment as review_sentiment
+FROM
+    raw_reviews
