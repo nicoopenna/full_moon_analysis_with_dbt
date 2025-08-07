2c42cb7ecf0b3d7346ff29856d56aeca6083bb0c
nspenna <nicolas.penna16@gmail.com>
2025-08-07 20:10:40 -0300

    Dims and facts creation, yml materialization default

---
+++ dbtlearn/models/fct/fct_reviews.sql
@@ -0,0 +1,18 @@
+{{
+    config(
+        materialized = 'incremental',
+        on_schema_change = 'fail'
+    )
+}}
+
+with src_reviews as (
+    select * from 
+    {{ ref("src_reviews") }}
+)
+select * 
+from src_reviews
+where 1=1 
+and review_text is not null
+{% if is_incremental() %}
+and review_date > coalesce((select max(review_date) from {{ this }}), '1900-01-01')
+{% endif %}
