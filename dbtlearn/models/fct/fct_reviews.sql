b83bd898e9977642384e7bb2dfcbcf184b7c2c56
nspenna <nicolas.penna16@gmail.com>
2025-08-09 19:17:25 -0300

    Packages and new keys using surrogate keys function. Documentation and docs serve

--- dbtlearn/models/fct/fct_reviews.sql
+++ dbtlearn/models/fct/fct_reviews.sql
@@ -9,7 +9,8 @@ with src_reviews as (
     select * from 
     {{ ref("src_reviews") }}
 )
-select * 
+select  {{ dbt_utils.generate_surrogate_key(['listing_id', 'review_date', 'reviewer_name', 'review_text']) }} as review_id,
+* 
 from src_reviews
 where 1=1 
 and review_text is not null
